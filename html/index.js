3f193bf8619f13a2816a5a4c0bd203b05260ccef
huanghuanlai <102535481@qq.com>
2016-10-15 16:06:00 +0800

    add cache

--- html/index.js
+++ html/index.js
@@ -1,6 +1,8 @@
 var app = require('koa')();
-var server = require('koa-static');
+var server = require('koa-static-cache');
 
-app.use(server(__dirname+'/'));
+app.use(server(__dirname+'/'),{
+	maxAge: 365 * 24 * 60 * 60
+});
 
 app.listen(8081);
